35310fbb833aef928dd8116cd222a24e6356d8a8
Leonardo Gigli <leonardo.gigli@thecrossproduct.com>
2024-07-15 17:02:20 +0200

    added class StandardizeIntensity

--- myria3d/pctl/transforms/transforms.py
+++ myria3d/pctl/transforms/transforms.py
@@ -133,6 +133,28 @@ class StandardizeRGB(BaseTransform):
         clamped = torch.clamp(input=standard, min=-clamp, max=clamp)
         return clamped
 
+class StandardizeIntensity(BaseTransform):
+    """Standardize RGB and log(Intensity) features."""
+
+    def __call__(self, data: Data):
+        idx = data.x_features_names.index("Intensity")
+        # Log transform to be less sensitive to large outliers - info is in lower values
+        data.x[:, idx] = torch.log(data.x[:, idx] + 1)
+        data.x[:, idx] = self.standardize_channel(data.x[:, idx])
+
+        return data
+
+    def standardize_channel(self, channel_data: torch.Tensor, clamp_sigma: int = 3):
+        """Sample-wise standardization y* = (y-y_mean)/y_std. clamping to ignore large values."""
+        mean = channel_data.mean()
+        std = channel_data.std() + 10**-6
+        if torch.isnan(std):
+            std = 1.0
+        standard = (channel_data - mean) / std
+        clamp = clamp_sigma * std
+        clamped = torch.clamp(input=standard, min=-clamp, max=clamp)
+        return clamped
+
 
 class StandardizeRGBAndIntensity(BaseTransform):
     """Standardize RGB and log(Intensity) features."""
